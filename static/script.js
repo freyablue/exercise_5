caadcf39947a1079725b169330ee4933079ca3c3
yijia <yijiac@uchicago.edu>
2024-02-09 13:10:23 -0600

    fst

--- static/script.js
+++ static/script.js
@@ -5,19 +5,46 @@
 // Automatically poll for new messages on a regular interval.
 // Allow changing the name of a room
 function postMessage() {
-  return;
+  fetch('/api/room/<room_id>/messages', {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json',
+      'API-Key':'your_api_key',
+    },
+    body: JSON.stringify({content: content}),
+  })
+  .then(response => response.json())
+  .then(result =>{
+    console.log("Message Posted: ", result);
+  })
+  .catch(error=> console.error('Error Posting Message ', error));
 }
 
 function getMessages() {
-  return;
+  fetch('/api/room/<room_id>/messages', {
+    method: 'GET',
+    headers: {
+        'API-Key': 'your_api_key',
+    },
+  })
+  .then(response => response.json())
+  .then(messages => {
+    // Process and display messages
+    console.log('Fetched Messages:', messages);
+  })
+  .catch(error => console.error('Error Fetching Messages:', error));
 }
 
 function startMessagePolling() {
-  return;
+  setInterval(function () {
+    getMessages();
+  }, 100);  // the interval
 }
 
-function updateRoomName() {
-  return;
+function updateRoomName(newName) {
+  fetch('/api/room/update-name', {
+    
+  })
 }
 
 /* For profile.html */
